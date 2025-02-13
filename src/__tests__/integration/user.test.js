d68753336481d6c9380b550699ea723739447316
Omosuyi Olawole <omooladev@gmail.com>
2025-02-13 16:42:14 +0100

    test the date of birth field

--- src/__tests__/integration/user.test.js
+++ src/__tests__/integration/user.test.js
@@ -1,35 +1,163 @@
+// jest.mock("../../models/user", () => {
+//   return {
+//     create: jest
+//       .fn()
+//       .mockResolvedValueOnce({ email: "test@example.com" })
+//       .mockRejectedValueOnce({ code: 11000 }),
+//   };
+// });
 const User = require("../../models/user");
 
 describe("User Model Tests", () => {
-  it("Should the following fields be required", async () => {
+  it("Should throw an Error if following fields are not supplied", async () => {
     const user = new User({});
-    try {
-      await user.validate();
-    } catch (validationErrors) {
-      expect(validationErrors.errors["password"]).toBeDefined();
-      expect(validationErrors.errors["email"]).toBeDefined();
-      expect(validationErrors.errors["phoneNumber"]).toBeDefined();
-      expect(validationErrors.errors["fullName"]).toBeDefined();
-    }
+    const validationErrors = user.validateSync();
+
+    expect(validationErrors.errors["password"]).toBeDefined();
+    expect(validationErrors.errors["email"]).toBeDefined();
+    expect(validationErrors.errors["phoneNumber"]).toBeDefined();
+    expect(validationErrors.errors["fullName"]).toBeDefined();
   });
-  it.only("Should the Fullname consist of the Last name and first name", async () => {
+  it("Should throw an Error if the fullname does not consist of the Last name and first name", async () => {
     const user = new User({
       fullName: "omosuyiolawole", //fullname must be a string of lastName and first Name seperated by a space e.g "Omosuyi Olawole"
       email: "test@gmail.com",
       password: "Test1234##",
       phoneNumber: 90,
     });
-    try {
-      await user.validate();
-    } catch (validationErrors) {
-      console.log(validationErrors);
-      expect(validationErrors.errors["fullName"]).toBeDefined();
-      expect(validationErrors.errors["fullName"].message).toBe("Please provide Last name and First name");
-    }
+    const validationErrors = user.validateSync();
+    expect(validationErrors.errors["fullName"]).toBeDefined();
+    expect(validationErrors.errors["fullName"].message).toBe("Please provide Last name and First name");
   });
+  describe("Email", () => {
+    it("Should throw an error if the email is invalid", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test-gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors.errors["email"]).toBeDefined();
+      expect(validationErrors.errors["email"].message).toBe("Please provide a valid email address");
+    });
+    it("Should pass if the email is valid", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors).not.toBeDefined();
+    });
+    it("Should throw an Error when email address already exist (i.e Check uniqueness of email address)", async () => {
+      await User.create({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+      });
+      try {
+        await User.create({
+          fullName: "Omosuyi Olawole",
+          email: "test@gmail.com",
+          password: "Test1234##",
+          phoneNumber: 90,
+        });
+      } catch (validationError) {
+        expect(validationError.code).toBe(11000); //where 11000 is mongodb uniqueness code error
+      }
+    });
+  });
+  describe("Password", () => {
+    it("Should throw an error if password is less than 8 characters", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "e",
+        phoneNumber: 90,
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors.errors["password"]).toBeDefined();
+      expect(validationErrors.errors["password"].message).toBe("Password must be at least 8 characters long");
+    });
+    it(`Should pass if password meets the security requirements which is 
+       "Password must contain at least one lowercase character, 
+       one uppercase character, and one number, symbol, or whitespace character",`, async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors).not.toBeDefined();
+    });
+    it("Should throw an Error when passwords does not meet the security requirements", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "est1234##",
+        phoneNumber: 90,
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors.errors["password"]).toBeDefined();
+      expect(validationErrors.errors["password"].message).toBe(
+        "Password must contain at least one lowercase character, one uppercase character, and one number, symbol, or whitespace character"
+      );
+    });
+  });
+
+  describe("Role", () => {
+    it("Should the user Role have a default value of fan when user creates an account", async () => {
+      const user = new User({});
+      expect(user.role).toBe("fan");
+    });
+    ["fan", "fighter", "promoter", "admin"].map((role) => {
+      it(`Should the ${role} be a valid user role`, () => {
+        const user = new User({
+          fullName: "Omosuyi Olawole",
+          email: "test@gmail.com",
+          password: "Test1234##",
+          phoneNumber: 90,
+          role,
+        });
+        const validationErrors = user.validateSync();
+        expect(validationErrors).not.toBeDefined();
+      });
+    });
+  });
+  describe("Date of Birth ", () => {
+    it("Should date of birth field be required if the user is a fighter", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+        role: "fighter",
+        dateOfBirth: "",
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors.errors["dateOfBirth"]).toBeDefined();
+      expect(validationErrors.errors["dateOfBirth"].message).toBe("Please provide date of birth");
+    });
+    it("Should date of birth field be optional if the user is not a fighter", async () => {
+      const user = new User({
+        fullName: "Omosuyi Olawole",
+        email: "test@gmail.com",
+        password: "Test1234##",
+        phoneNumber: 90,
+        role: "fan",
+        dateOfBirth: "",
+      });
+      const validationErrors = user.validateSync();
+      expect(validationErrors).not.toBeDefined();
+    });
+  });
+
   it("Should the following fields have a default value when user creates an account", async () => {
     const user = new User({});
-    expect(user.role).toBe("fan");
     expect(user.emailIsVerified).toBe(false);
     expect(user.userIsVerified).toBe(false);
   });
