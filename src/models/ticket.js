407afa5bed64f306af4887848b257424d0a8fd97
Omosuyi Olawole <omooladev@gmail.com>
2025-02-13 10:03:48 +0100

    updated the ticket schema with all other fields

--- src/models/ticket.js
+++ src/models/ticket.js
@@ -1,5 +1,21 @@
 const mongoose = require("mongoose");
 
-const TicketSchema = new mongoose.Schema({});
+const TicketSchema = new mongoose.Schema(
+  {
+    eventId: { type: mongoose.Schema.Types.ObjectId, ref: "Event" },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    qrCodeImageUrl: { type: String, required: ["Please provide url for the QrCode image"] },
+    payment: {
+      amount: { type: Number, required: [true, "Please provide amount paid"] },
+      transactionId: { type: String, required: [true, "Please provide transaction id of payment"] },
+      status: {
+        type: String,
+        enum: ["processing", "failed", "success"],
+        required: [true, "Please provide payment id"],
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Ticket", TicketSchema);
